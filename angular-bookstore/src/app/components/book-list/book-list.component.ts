430f109af282457a1e49b8af19bc0466737cbac5
Nusrat <njahan8@centennialcollege.ca>
2020-03-28 21:17:17 -0400

    completing feature 2

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -4,7 +4,8 @@ import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  //templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
