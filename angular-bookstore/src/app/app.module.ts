796c38c0d4722d4a1c5e5a453ffd3d1f6a51cbd9
Nusrat <njahan8@centennialcollege.ca>
2020-06-13 01:14:46 -0400

    Completed feature 9 - displaying cart-items

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -14,8 +14,10 @@ import { BookDetailsComponent } from './components/book-details/book-details.com
 //import { JwPaginationComponent } from 'jw-angular-pagination';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
+  { path: 'cart-details', component: CartDetailsComponent},
   { path: 'books/:id', component: BookDetailsComponent},
   { path: 'books', component: BookListComponent},
   { path: 'search/:keyword', component: BookListComponent},
@@ -32,7 +34,8 @@ const routes: Routes = [
     BookCategoryComponent,
     SearchComponent,
     BookDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
    // JwPaginationComponent
   ],
   imports: [
---
+++ angular-bookstore/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private _cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartDetails();
+  }
+  cartDetails() {
+    this.cartItems = this._cartService.cartItems;
+
+    // subscribe to the events
+    this._cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+    this._cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+    this._cartService.calculateTotalPrice();
+  }
+
+}
